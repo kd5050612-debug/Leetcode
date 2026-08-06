5bf99c2c67c846dd95adcbef97ea87d2def675ea
Krishna Das <kd5050612@gmail.com>
2026-08-06 17:07:23 +0530

    Create Smallest Divisible Digit Product I.java

---
+++ Smallest Divisible Digit Product I.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+
+        int current = n;
+
+        while (true) {
+
+            int product = 1;
+            int num = current;
+
+            while (num > 0) {
+                int digit = num % 10;
+                product *= digit;
+                num /= 10;
+            }
+
+            if (product % t == 0) {
+                return current;
+            }
+
+            current++;
+        }
+    }
+}
